ba72eed75241551008854f4c6db86b95b2899207
Adishwar Rishi <adiswa123@gmail.com>
2023-08-16 14:35:05 +1000

    Remove optional auth config from the project

--- src/routes/v8/index.ts
+++ src/routes/v8/index.ts
@@ -6,11 +6,7 @@ import { artifactRouter } from './artifacts';
 export const v8App = new Hono<{ Bindings: Env }>();
 
 v8App.use('/artifacts/*', async (c, next) => {
-  if (c.env.REQUIRE_AUTH) {
-    const middleware = bearerAuth({ token: c.env.TURBO_TOKEN });
-    await middleware(c, next);
-  } else {
-    await next();
-  }
+  const middleware = bearerAuth({ token: c.env.TURBO_TOKEN });
+  await middleware(c, next);
 });
 v8App.route('/artifacts', artifactRouter);
