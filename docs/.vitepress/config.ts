f324034180a78089b619c3704c2ee31616715f24
Adishwar Rishi <adiswa123@gmail.com>
2024-01-20 22:21:27 +1100

    Setup vitepress

---
+++ docs/.vitepress/config.ts
@@ -0,0 +1,27 @@
+import { defineConfig } from 'vitepress';
+
+// https://vitepress.dev/reference/site-config
+export default defineConfig({
+  title: 'Turborepo Remote Cache Cloudflare',
+  description:
+    'An implementation of the turborepo-remote-cache server custom made for Cloudflare Workers',
+  themeConfig: {
+    // https://vitepress.dev/reference/default-theme-config
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Examples', link: '/markdown-examples' },
+    ],
+
+    sidebar: [
+      {
+        text: 'Examples',
+        items: [
+          { text: 'Markdown Examples', link: '/markdown-examples' },
+          { text: 'Runtime API Examples', link: '/api-examples' },
+        ],
+      },
+    ],
+
+    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }],
+  },
+});
---
+++ docs/index.md
@@ -0,0 +1,24 @@
+---
+# https://vitepress.dev/reference/default-theme-home-page
+layout: home
+
+hero:
+    name: 'Turborepo Remote Cache Cloudflare'
+    text: 'An implementation of the turborepo-remote-cache server custom made for Cloudflare Workers'
+    tagline: My great project tagline
+    actions:
+        - theme: brand
+          text: Markdown Examples
+          link: /markdown-examples
+        - theme: alt
+          text: API Examples
+          link: /api-examples
+
+features:
+    - title: Feature A
+      details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+    - title: Feature B
+      details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+    - title: Feature C
+      details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+---
