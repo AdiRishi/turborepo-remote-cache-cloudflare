c17d4324294dc26800feb4f712965857a68759e9
Adishwar Rishi <adiswa123@gmail.com>
2024-07-21 22:07:28 +1000

    feat: Upgrade to eslint v9 (#577)
    * feat: Upgrade to eslint v9
    
    * Upgrade to typescript-eslint v8 alpha

---
+++ eslint.config.mjs
@@ -0,0 +1,56 @@
+import js from '@eslint/js';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
+import tsEslint from 'typescript-eslint';
+
+/**
+ * @type {import('eslint').Linter.FlatConfig[]}
+ */
+export default [
+  js.configs.recommended,
+  ...tsEslint.configs.recommendedTypeChecked,
+  ...tsEslint.configs.stylisticTypeChecked,
+  eslintPluginPrettier,
+  eslintConfigPrettier,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/consistent-type-definitions': 'off',
+    },
+  },
+  {
+    files: ['docs/**/*.ts'],
+    ...tsEslint.configs.disableTypeChecked,
+  },
+  {
+    ignores: [
+      '**/.eslintrc.json',
+      '**/node_modules',
+      '**/.nuxt',
+      '**/.output',
+      '**/.turbo',
+      '**/assets',
+      'functions/*.js',
+      'functions/*.js.*',
+      '**/public',
+      '**/dist',
+      '**/coverage',
+      '**/.wrangler',
+      '**/.prettierrc.js',
+    ],
+  },
+];
--- vitest-env.d.ts
+++ vitest-env.d.ts
@@ -1,6 +1,6 @@
 import { type Env } from './src/index';
 
 declare module 'cloudflare:test' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   interface ProvidedEnv extends Env {}
 }
