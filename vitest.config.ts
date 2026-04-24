c6f4f698df4e561d572dc5e78c35fb9a151b77fb
Adishwar Rishi <adiswa123@gmail.com>
2026-04-25 00:05:58 +1000

    fix: increase test timeout in CI

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,8 @@
 import { cloudflareTest } from '@cloudflare/vitest-pool-workers';
 import { defineConfig } from 'vitest/config';
 
+const ciTimeout = process.env.CI ? 15_000 : undefined;
+
 export default defineConfig({
   resolve: {
     tsconfigPaths: true,
@@ -20,6 +22,8 @@ export default defineConfig({
     }),
   ],
   test: {
+    hookTimeout: ciTimeout,
+    testTimeout: ciTimeout,
     reporters: ['verbose'],
     coverage: {
       provider: 'istanbul',
